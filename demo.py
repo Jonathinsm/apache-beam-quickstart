9eae2d28eb3c2e497882667099f745e0f0fcd583
jsimanca <jonathan.simanca@mercadolibre.com.co>
2021-09-27 12:00:32 -0500

    demo para mostrar mejorado

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
 import apache_beam as beam
 
-p = beam.Pipeline()
+def filtering(record):
+    return record[4] == 'NJ'
 
-count = {
-    p
-    | beam.io.ReadFromText('data/addresses.csv')
-    | beam.Map(lambda element: element.split(','))
-    | beam.Map(print)
-}
+p = beam.Pipeline()
+lines = p | beam.io.ReadFromText('data/addresses.csv')
+splited = lines | beam.Map(lambda element: element.split(','))
+filtered = splited | beam.Filter(filtering)
+formated = filtered | beam.Map(lambda record: (record[0],1))
+counted = formated | beam.CombinePerKey(sum)
+counted | beam.Map(print)
 
 p.run()
